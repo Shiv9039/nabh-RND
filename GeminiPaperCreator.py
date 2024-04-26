10b9a03d665c8a929d89e453911358b9497e2c09
Shivendra Patel <64832314+Shiv9039@users.noreply.github.com>
2024-04-26 13:21:11 +0530

    Add files via upload

---
+++ GeminiPaperCreator.py
@@ -0,0 +1,63 @@
+from langchain_community.document_loaders import PyPDFLoader
+from fastapi import FastAPI , Request , File, UploadFile
+import google.generativeai as genai
+import uuid
+import os
+import tempfile
+
+genai.configure(api_key=str(os.environ['api_key']))
+
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 0,
+  "max_output_tokens": 8192,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+
+app = FastAPI()
+
+@app.post("/create_paper")
+async def load_pdf(file: UploadFile = File(...)):
+      try:
+
+          with tempfile.NamedTemporaryFile(delete=True) as tmp:
+              tmp.write(await file.read())
+              tmp_file_path = tmp.name
+              loader = PyPDFLoader(tmp_file_path, extract_images=True)
+              data = loader.load()
+
+          model = genai.GenerativeModel(model_name="gemini-1.0-pro-latest",
+                                        generation_config=generation_config,
+                                        safety_settings=safety_settings)
+
+          prompt = f"""persona - you are a question paper creater 
+                       goal - your job is to look into provided context and create mcq test for students with only exact data as provided also output json with question no. and right option.
+                       context - {data}"""
+
+          convo = model.start_chat()
+          convo.send_message(prompt)
+
+          return {"data" : convo.last.text}
+
+      except Exception as e:
+          return {"data" : e}
+
